e95f29f592007318e76fd196a5d861a616506b38
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-04-04 00:33:12 -0500

    Update DeepClone.java

--- Java/Deep Cloner/DeepClone.java
+++ Java/Deep Cloner/DeepClone.java
@@ -29,12 +29,9 @@ public class DeepClone{
                 copy[i] = deepCopy(decon[i]);
             }//for(int i=0;i<decon.length;i++){
         }//if(decon[0].getClass().isArray()){
-        else {
-            copy = decon.clone();
-        }//else{
         E recon = (E) copy;
         return recon;
-    }//public static <E> E deepCopyTest (E original){
+    }//public static <E> E deepCopy (E original){
   
   //the generic (non primitive) will except an array of any number of dimensions as long as it does not contain primitives as the array type (may contain primitives in cloned object)
   //new primitives can be added by copying the existing ones and changing the primitive array type 
