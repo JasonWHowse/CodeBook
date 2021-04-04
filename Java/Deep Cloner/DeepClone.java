ebf92702e8ee6fdcf69b869df28ee9be06e06b09
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-04-03 23:59:46 -0500

    Update DeepClone.java

--- Java/Deep Cloner/DeepClone.java
+++ Java/Deep Cloner/DeepClone.java
@@ -7,55 +7,38 @@ package helper_functions;
 import java.lang.reflect.Array;
 import java.util.Collection;
 
-public class DeepClone{
-    public static <E> E[][] deepCopy (E[][] original){
-        E[][] copy= original.clone();
-        for(int i=0;i<original.length;i++){
-            copy[i]=original[i].clone();
-        }//for(int i=0;i<original.length;i++){
-        return copy;
-    }//public static <E> E[][] deepCopy (E[][] original){
-
-    public static <E> E[][][] deepCopy (E[][][] original){
-        E[][][] copy= original.clone();
-        for(int i=0;i<original.length;i++){
-            copy[i]=deepCopy(original[i]).clone();
-        }//for(int i=0;i<original.length;i++){
-        return copy;
-    }//public static <E> E[][][] deepCopy (E[][][] original){
-
-    public static int[][] deepCopy (int[][] original){
-        int[][] copy= original.clone();
-        for(int i=0;i<original.length;i++){
-            copy[i]=original[i].clone();
+public class DeepClone{   
+    public static int[][] deepCopy(int[][] original) {
+        int[][] copy = original.clone();
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = original[i].clone();
         }//for(int i=0;i<original.length;i++){
         return copy;
     }//public static int[][] deepCopy (int[][] original){
 
-    public static int[][][] deepCopy (int[][][] original){
-        int[][][] copy= original.clone();
-        for(int i=0;i<original.length;i++){
-            copy[i]=deepCopy(original[i]).clone();
-        }//for(int i=0;i<original.length;i++){
-        return copy;
-    }//public static int[][][] deepCopy (int[][][] original){
-
-    public static boolean[][] deepCopy (boolean[][] original){
-        boolean[][] copy= original.clone();
-        for(int i=0;i<original.length;i++){
-            copy[i]=original[i].clone();
+    public static boolean[][] deepCopy(boolean[][] original) {
+        boolean[][] copy = original.clone();
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = original[i].clone();
         }//for(int i=0;i<original.length;i++){
         return copy;
     }//public static boolean[][] deepCopy (boolean[][] original){
 
-    public static boolean[][][] deepCopy (boolean[][][] original){
-        boolean[][][] copy= original.clone();
-        for(int i=0;i<original.length;i++){
-            copy[i]=deepCopy(original[i]).clone();
-        }//for(int i=0;i<original.length;i++){
-        return copy;
-    }//public static boolean[][][] deepCopy (boolean[][][] original){
+    public static <E> E deepCopy(E original) {
+        E[] decon = (E[]) original;
+        E[] copy = decon.clone();
+        if (decon[0].getClass().isArray()) {
+            for (int i = 0; i < decon.length; i++) {
+                copy[i] = deepCopy(decon[i]);
+            }//for(int i=0;i<decon.length;i++){
+        }//if(decon[0].getClass().isArray()){
+        else {
+            copy = decon.clone();
+        }//else{
+        E recon = (E) copy;
+        return recon;
+    }//public static <E> E deepCopyTest (E original){
   
-  //any of the 3 dimension methods can be expanded by simply adding a new method with an additional dimension to the type, there must be type for each dimension smaller as well, a 5 dimension array would also need the 4 dimension added as well as containing the 3 and 2 dimension methods
+  //the generic (non primitive) will except an array of any number of dimensions as long as it does not contain primitives as the array type (may contain primitives in cloned object)
   //new primitives can be added by copying the existing ones and changing the primitive array type 
 }//public class DeepClone{
