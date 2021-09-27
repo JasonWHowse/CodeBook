56eda4ddd595b33dce2151b755eea1928492a286
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-09-27 16:53:18 -0500

    Create SearchSysIDRecord.js

---
+++ ServiceNow/Scripts/SearchAll/SearchSysIDRecord.js
@@ -0,0 +1,20 @@
+function SearchSysIDRecord(querySysID) {
+  var gr = new GlideRecord('sys_db_object');
+  gr.addQuery('sys_update_nameISNOTEMPTY');
+  gr.query();
+  while(gr.next()) {
+    try{
+      if(
+        gr.name.toString() != '' &&
+        gr.name.toString() != 'sn_m2m_note_template_for_table' &&
+        gr.name.toString() != 'sn_templated_snip_note_template' &&
+        gr.name.toString() != 'sys_kmf_instance_key'
+      ){
+        var gr2 = new GlideRecord(gr.name.toString());
+        if(gr2.get('sys_id', querySysID)){
+          gr2;  
+        }
+      }
+    }catch(error){}
+  }
+}
