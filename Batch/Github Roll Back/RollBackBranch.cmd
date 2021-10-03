12b1da69fc2c3a439390d74903ec966755d1d08e
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-10-03 18:00:22 -0500

    Update RollBackBranch.cmd

--- Batch/Github Roll Back/RollBackBranch.cmd
+++ Batch/Github Roll Back/RollBackBranch.cmd
@@ -1,7 +1,7 @@
 rem first variable is the project name, second variable is the link to the repo, 3rd variable hash of the commit to roll back to, 4th variable Branch to roll back
 mkdir gitRollbkdir
 cd gitRollbkdir
-git clone %2
+git clone --branch %4 %2
 cd %1
 git reset --hard %3
 git push -f origin %4
