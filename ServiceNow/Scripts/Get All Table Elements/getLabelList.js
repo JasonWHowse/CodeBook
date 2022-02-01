25bba872a25cb06da977ff0b76b44f44c00f0d7a
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2022-02-01 14:20:16 -0600

    Update getLabelList.js

--- ServiceNow/Scripts/Get All Table Elements/getLabelList.js
+++ ServiceNow/Scripts/Get All Table Elements/getLabelList.js
@@ -1,4 +1,24 @@
 function getLabelList(table) {
+    var tableList = "";
+    var searchTable = table;
+    var hasNext = true;
+    while (hasNext) {
+        var gr = new GlideRecord('sys_db_object');
+        gr .addQuery('name=' + searchTable);
+        tableList += ((tableList.length == 0) ? "^name=" : "^ORname=") + searchTable;
+        gr.query();
+
+        if (gr.next()) {
+            if (gr.super_class.name) {
+                searchTable = gr.super_class.name;
+            } else {
+                hasNext = false;
+            }
+        } else {
+            hasNext = false;
+        }
+    }
+
     var gr = new GlideRecord(table);
     gr.query();
     if (gr.next()) {
@@ -6,7 +26,7 @@ function getLabelList(table) {
         var output = "Labels and Elements for " + table + "\r\n";
         for (var i = 0; i < keyList.length; i++) {
             var gr2 = new GlideRecord('sys_documentation');
-            gr2.addQuery('element=' + keyList[i] + '^name=' + table);
+            gr2.addQuery('element=' + keyList[i] + tableList);
             gr2.query();
             if (gr2.next()) {
                 output += "Label: " + gr2.label + " Element: " + keyList[i] + '\r\n';
